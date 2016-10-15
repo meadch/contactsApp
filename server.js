14905fda34a2dc31e86ed9fda27f384c6bcc1639
meadch <meadch@gmail.com>
2016-10-14 17:55:10 -0700

    updates static and dependencies

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ var express = require('express'),
     app = express(),
     port = 8001;
 
-app.use('/contacts', express.static(__dirname + "/app"));
-app.use('/contacts', express.static(__dirname + "/bower_components"));
+app.use(express.static(__dirname + "/app"));
+app.use(express.static(__dirname + "/bower_components"));
 
 app.get('/', function (req, res){
   res.sendFile(__dirname + '/app/index.html');
